f7bbb308e0464cf5554c6ffaee734f0c02d95ded
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-28 14:48:24 +0100

    Combine Arrays with the Spread Operator
    Another huge advantage of the spread operator, is the ability to combine arrays, or to insert all the elements of one array into another, at any index. With more traditional syntaxes, you can concatenate arrays, but this only allows you to combine arrays at the end of one, and at the start of another. Spread syntax makes the following operation extremely simple:
    
    let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
    
    let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
    // thatArray now equals ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
    Using spread syntax, you have just achieved an operation that would have been more complex and more verbose had you used traditional methods.

---
+++ Combining-Arrays-with-the-Spread-Operator.js
@@ -0,0 +1,8 @@
+// function spreadOut returns the variable sentence, modify the function using the spread operator 
+// so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
+
+function spreadOut() {
+let fragment = ['to', 'code'];
+let sentence = ['learning',...fragment, 'is', 'fun'];
+
+console.log(spreadOut());

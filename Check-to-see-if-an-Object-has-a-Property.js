84cbcaadeaf047983d6ce51ea6b73a8b4bac2989
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-06-04 15:23:05 +0100

    Check if an Object has a Property
    We can add, modify, and remove keys from objects. But what if we just wanted to know if an object has a specific property? JavaScript provides us with two different ways to do this. One uses the hasOwnProperty() method and the other uses the in keyword. If we have an object users with a property of Alan, we could check for its presence in either of the following ways:
    
    users.hasOwnProperty('Alan');
    'Alan' in users;
    // both return true

---
+++ Check-to-see-if-an-Object-has-a-Property.js
@@ -0,0 +1,33 @@
+// The object, users, has some users in it and a function isEveryoneHere, which we pass the users object to as an argument. 
+// Finish writing this function so that it returns true only if the users object contains all four names, 
+// Alan, Jeff, Sarah, and Ryan, as keys, and false otherwise.
+
+let users = {
+Alan: {
+age: 27,
+online: true,
+}
+
+Jeff {
+age: 32,
+online: true,
+}
+
+Sarah {
+age: 48,
+online: true,
+}
+
+Ryan
+age: 19,
+online: true,
+}
+};
+
+function isEverywhere(obj) {
+
+if(users.hasOwnProperty('Alan', 'Jeff', 'Sarah', 'Ryan')) {
+return true;
+}
+return false;
+

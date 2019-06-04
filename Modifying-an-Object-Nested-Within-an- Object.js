ece50e1d78dfc6fe1c6c05f906ec755c455261fb
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-06-04 13:23:51 +0100

    Modify an Object Nested Within an Object
    Object properties can be nested to an arbitrary depth, and their values can be any type of data supported by JavaScript, including arrays and even other objects. Consider the following:
    
    let nestedObject = {
      id: 28802695164,
      date: 'December 31, 2016',
      data: {
        totalUsers: 99,
        online: 80,
        onlineStatus: {
          active: 67,
          away: 13
        }
      }
    };
    nestedObject has three unique keys: id, whose value is a number, date whose value is a string, and data, whose value is an object which has yet another object nested within it. While structures can quickly become complex, we can still use the same notations to access the information we need

---
+++ Modifying-an-Object-Nested-Within-an- Object.js
@@ -0,0 +1,15 @@
+// Properties can be modified using the dot or bracket notation. Set the value of the online key to 45.
+
+let userActivity = {
+
+id: 24398506431,
+date: June 4th 2019,
+data: {
+totalUsers: 51,
+online: 42
+}
+};
+
+userActivity.data.online = 45;
+
+console.log(userActivity);
